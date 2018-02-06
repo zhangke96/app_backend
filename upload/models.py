7cd4ae0e00bf2281c97c5c76b0bd86771e40fbe1
zhangke <zhangke960808@163.com>
2018-02-06 22:48:39 +0800

    UploadFile model creator foreign key modify

--- upload/models.py
+++ upload/models.py
@@ -3,7 +3,7 @@ from auth_system.models import MyUser
 
 class UploadFile(models.Model):
     id = models.AutoField('文件id', primary_key=True)
-    creator = models.ForeignKey(MyUser, related_name='UploadFile_creator')
+    creator = models.ForeignKey(MyUser, related_name='UploadFile_creator', on_delete=models.SET_NULL, null=True)
     upload_time = models.DateTimeField(auto_now_add=True)
     description = models.CharField('文件描述', max_length=1024)
     filename = models.CharField('文件名', max_length=1024)
